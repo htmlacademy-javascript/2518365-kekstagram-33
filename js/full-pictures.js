f32f5124f4171be33db9d22c8cbef53e71da3947
Alexey <kiselev-academy@yandex.ru>
2024-11-18 18:32:44 +0300

    Opens and closes

--- js/full-pictures.js
+++ js/full-pictures.js
@@ -1,8 +1,11 @@
 import { isEscapeKey } from './util.js';
 
+const COMMENTS_COUNT = 5;
+
 const fullPhotoElement = document.querySelector('.big-picture');
 const closeBtnElement = document.querySelector('.big-picture__cancel');
 const commentsListElement = fullPhotoElement.querySelector('.social__comments');
+const loadBtnElement = fullPhotoElement.querySelector('.comments-loader');
 
 const renderComment = ({avatar, name, message}) =>
   `<li class="social__comment">
@@ -14,32 +17,56 @@ const renderComment = ({avatar, name, message}) =>
     <p class="social__text">${message}</p>
   </li>`;
 
+const renderCurrentComments = (maxIndex, comments) => {
+  let currentIndex = commentsListElement.children.length;
+  if (maxIndex >= comments.length) {
+    maxIndex = comments.length;
+    loadBtnElement.classList.add('hidden');
+  }
+  while (currentIndex < maxIndex) {
+    const comment = renderComment(comments[currentIndex]);
+    commentsListElement.insertAdjacentHTML('beforeend', comment);
+    currentIndex++;
+  }
+};
+
+let onloadCommentsBtnClick;
+
 const renderComments = (comments) => {
-  commentsListElement.innerHTML = '';
+  let maxIndex = COMMENTS_COUNT;
 
   comments.forEach((item) => {
     const comment = renderComment(item);
     commentsListElement.insertAdjacentHTML('beforeend', comment);
   });
+
+  onloadCommentsBtnClick = () => {
+    renderCurrentComments(maxIndex, comments);
+    fullPhotoElement.querySelector('.social__comment-shown-count').textContent = maxIndex > comments.length ? comments.length : maxIndex;
+    maxIndex += COMMENTS_COUNT;
+  };
+
+  commentsListElement.innerHTML = '';
+  loadBtnElement.classList.remove('hidden');
+  onloadCommentsBtnClick();
+  loadBtnElement.addEventListener('click', onloadCommentsBtnClick);
 };
 
 const renderFullPhoto = ({url, likes, comments, description}) => {
   fullPhotoElement.querySelector('img').src = url;
   fullPhotoElement.querySelector('.likes-count').textContent = likes;
-  fullPhotoElement.querySelector('.social__comment-shown-count').textContent = comments.length;
   fullPhotoElement.querySelector('.social__comment-total-count').textContent = comments.length;
   fullPhotoElement.querySelector('.social__caption').textContent = description;
 
   renderComments(comments);
 
-  fullPhotoElement.querySelector('.social__comment-count').classList.add('hidden');
-  fullPhotoElement.querySelector('.comments-loader').classList.add('hidden');
 };
 
 const closeFullPhoto = () => {
   fullPhotoElement.classList.add('hidden');
 
   document.body.classList.remove('modal-open');
+  loadBtnElement.removeEventListener('click', onloadCommentsBtnClick);
   closeBtnElement.removeEventListener('click', closeFullPhoto);
   document.removeEventListener('keydown', onEscapeBtnKeydown);
 };
@@ -59,6 +86,5 @@ const openFullPhoto = (photo) => {
   closeBtnElement.addEventListener('click', closeFullPhoto);
   document.addEventListener('keydown', onEscapeBtnKeydown);
 };
-
 
 export { openFullPhoto };
