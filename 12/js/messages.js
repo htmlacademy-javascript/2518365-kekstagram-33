6fdc92f3ea2cb6e624bf293025ac98833b52501a
Keks <keks@htmlacademy.ru>
2024-11-25 19:22:37 +0300

    :heavy_check_mark: Сборка #12

--- 12/js/messages.js
+++ 12/js/messages.js
@@ -1,4 +1,4 @@
-import { isEscapeKey } from './util';
+import { isEscapeKey } from './util.js';
 
 const successMessageElement = document.querySelector('#success').content.querySelector('.success');
 const errorMessageElement = document.querySelector('#error').content.querySelector('.error');
