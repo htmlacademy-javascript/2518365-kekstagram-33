9448964a7600e4de407f35094e8b90d56aaaa9a0
Alexey <kiselev-academy@yandex.ru>
2024-11-28 14:55:56 +0300

    Mother of pearl buttons

--- js/main.js
+++ js/main.js
@@ -1,13 +1,12 @@
 import { renderPhotos } from './pictures.js';
 import './form.js';
 import { getData } from './server.js';
-import {showDataErrorMessage} from './util.js';
-
-let photos = [];
+import { showDataErrorMessage } from './util.js';
+import { applyFilter } from './filter.js';
 
 const addPhotos = (data) => {
-  photos = data.slice();
-  renderPhotos(photos);
+  renderPhotos(data);
+  applyFilter(data);
 };
 
 getData(addPhotos, showDataErrorMessage);
